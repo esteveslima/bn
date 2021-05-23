20d84da1be120c09a1229df9f5c9fba4209cdc9e
esteveslima <esteves_m_lima@hotmail.com>
2021-05-23 01:21:33 -0300

    fix(@services/users-activities): rename dao to kebab-case

--- packages/services/users-activities/src/common/database/dao/activities-dao.ts
+++ packages/services/users-activities/src/common/database/dao/activities-dao.ts
@@ -1,5 +1,5 @@
-export { default as createActivity } from './activities/createActivity';
-export { default as getActivity } from './activities/getActivity';
-export { default as updateActivity } from './activities/updateActivity';
-export { default as deleteActivity } from './activities/deleteActivity';
-export { default as listActivities } from './activities/listActivities';
+export { default as createActivity } from './activities/create-activity';
+export { default as getActivity } from './activities/get-activity';
+export { default as updateActivity } from './activities/update-activity';
+export { default as deleteActivity } from './activities/delete-activity';
+export { default as listActivities } from './activities/list-activities';
